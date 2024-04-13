57969927a447178e8e04de2116d7b42b053ae697
Aetherinox <aetherinox@proton.me>
2024-04-13 03:40:25 -0700

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,137 @@
-# xsumjs
-Checksum verification and digest validation for NodeJS
+# xSumJS
+
+[![Build Status](https://github.com/Aetherinox/xsumjs/workflows/CI/badge.svg)](https://github.com/Aetherinox/xsumjs/actions?query=workflow%3ACI)
+[![Code Coverage](https://codecov.io/gh/Aetherinox/xsumjs/branch/main/graph/badge.svg)](https://codecov.io/gh/Aetherinox/xsumjs)
+[![NPM package](https://img.shields.io/npm/v/xsumjs)](https://npm.im/xsumjs)
+
+xSumJS is a Node.js package which can be used for validating file checksums and comparing them against a hash digest file. Hash digests and checksums can be generated using any of the following programs:
+
+-   [xSum](https://github.com/Aetherinox/xsum-hash-utility)
+-   [sha256sum](https://help.ubuntu.com/community/HowToSHA256SUM)
+-   [DirHash](https://idrassi.github.io/DirHash/)
+-   [CyberChef](https://gchq.github.io/CyberChef/)
+
+<br />
+
+---
+
+<br />
+
+## Requirements
+
+`xsumjs` is tested with Node.js 16 (LTS) and higher
+
+<br />
+
+---
+
+<br />
+
+## Usage
+The following is a simple example of how to use:
+
+```javascript
+const xs = require('xsumjs');
+
+try
+{
+    await xs( algo, digest, baseDir, files );
+    console.debug( 'Good Checksum' );
+}
+catch ( err )
+{
+    console.error( 'Bad Checksum', err );
+}
+```
+
+<br />
+
+This is a more advanced example:
+
+```javascript
+
+const xs            = require('xsumjs');
+let fileList        = []; // populate fileList with your files to check
+let xsParams        = { encoding: 'binary' }; // binary, utf8, etc.
+
+await runValidator  ({ algo: 'sha256', fileList, fileDigest: 'SHA256SUM.txt', xsParams });
+
+async function runValidator( args )
+{
+    console.log( `Validating checksums for files against ${ args.fileDigest }` );
+
+    const digest        = path.join( '/path/to/', args.fileDigest );
+    const checksum      = new xs.CSVerify( args.algo, digest, args.xsParams );
+
+    await checksum.verify( '/path/base/folder/', args.fileList ).catch( err =>
+    {
+        
+        if ( err instanceof xs.CSErrorMismatch )
+        {
+            console.error( `Checksum for ${ err.filename } did not match digest ${ args.fileDigest }` );
+        }
+        else if ( err instanceof xs.CSErrorParse )
+        {
+            console.error( `Digest ${ args.fileDigest } could not be loaded`, err );
+        }
+        else if ( err instanceof xs.CSErrorNoMatch )
+        {
+            console.error( `Checksum for ${ err.filename } not found in digest ${ args.fileDigest }` );
+        }
+        else
+        {
+            console.error( `Error finding checksums in ${ args.fileDigest }`, err );
+        }
+
+        process.exit( 1 );
+    } );
+
+    console.log( `All files successfully validated with digest ${ args.fileDigest }` );
+}
+```
+
+<br />
+
+---
+
+<br />
+
+## Hash Digests
+A hash digest typically contains a list of all files you wish to track in your project to the left, formatted as the path to the file relative to your project folder, and then the checksum to the right.
+
+<br />
+
+```
+d63ba16a664619c2dc4eb2aeef2a2e64cbc7931b831e0adf1c2275ee08e8fd47  example_file_1.txt
+dfb8dacbd53eac730814ef2e9f74a47efabe5cb2a5e458bcad6380ae4c1f1f59  example_file_2.txt
+9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08  sample_zip_1.zip
+60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752  README.md
+```
+
+<br />
+
+---
+
+<br />
+
+## Development
+
+Install `eslint`, `prettier`, and ensure files are properly formatted:
+
+```shell
+npm install -g prettier
+npm install eslint --save-dev
+npx prettier --write .
+```
+
+<br />
+
+> [!NOTE]
+> Note that checksums are compared to `sha256` with `64 rounds`. Checksums double-checked with cyberchef.
+
+<br />
+
+To run tests:
+```shell
+npm run test
+```
---
+++ test/index.ts
@@ -0,0 +1,154 @@
+/*
+    MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+import path from 'path';
+import xsumjs, { CSErrorParse, CSErrorMismatch, CSVerify, CSErrorNoMatch } from '../src/';
+import test from 'ava'; // eslint-disable-line import/no-unresolved
+
+/*
+    Get fixture associated to test
+*/
+
+function getFileFixture(file: string): string {
+    return path.join(__dirname, 'fixtures', file);
+}
+
+/*
+    Check Checksum
+        fileDigest, filesToCheck
+*/
+
+function runTestChecksum(digest: string, files: string[] | string): Promise<void> {
+    return xsumjs('sha256', getFileFixture(digest), getFileFixture(''), files);
+}
+
+/*
+    Check valid checksum
+    should return valid checksum
+*/
+
+test('Digest checksum validation: Normal file', async (t) => {
+    await t.notThrowsAsync(runTestChecksum('tests-checksum.digest', 'tests-checksum-good'));
+});
+
+/*
+    Check valid checksum > binary
+    should return valid checksum
+*/
+
+test('Digest checksum validation:  Binary file', async (t) => {
+    await t.notThrowsAsync(runTestChecksum('tests-checksum.digest', 'tests-checksum.bin'));
+});
+
+/*
+    Open "fake" sha checksum file which doesnt exist and compare with file.
+    fake checksum doesnt exist, so it should return an error
+*/
+
+test('Fake file checksum doesnt match good file', async (t) => {
+    await t.throwsAsync(runTestChecksum('fake.sha256sum', 'tests-checksum-good'), {
+        instanceOf: Error,
+        code: 'ENOENT'
+    });
+});
+
+/*
+    Attempt to parse a checksum digest file that doesn't exist
+*/
+
+test('Checksum file parse unsuccessful', async (t) => {
+    const error = (await t.throwsAsync(
+        runTestChecksum('invalid.sha256sum', 'tests-checksum-good'),
+        {
+            instanceOf: CSErrorParse,
+            message: 'Could not parse checksum file at line #1: invalid'
+        }
+    )) as CSErrorParse;
+
+    t.is(error.lineNum, 1);
+    t.is(error.line, 'invalid');
+});
+
+/*
+    Could not locate a file checksum in the digest file
+*/
+
+test('Specified file not found in digest', async (t) => {
+    const error = (await t.throwsAsync(runTestChecksum('tests-checksum.digest', 'fake'), {
+        instanceOf: CSErrorNoMatch,
+        message: 'No checksum found in digest for file: "fake".'
+    })) as CSErrorNoMatch;
+
+    t.is(error.file, 'fake');
+});
+
+/*
+    File checksum mismatch with digest checksum entry
+*/
+
+test('Specified file checksum mismatch in digest', async (t) => {
+    const error = (await t.throwsAsync(
+        runTestChecksum('tests-checksum.digest', 'tests-checksum-bad'),
+        {
+            instanceOf: CSErrorMismatch,
+            message: '"tests-checksum-bad" does not have matching checksum'
+        }
+    )) as CSErrorMismatch;
+
+    t.is(error.file, 'tests-checksum-bad');
+});
+
+/*
+    Multiple files specified, at least one checksum mismatch
+*/
+
+test('Multiple files provided, one mismatch in digest', async (t) => {
+    const error = (await t.throwsAsync(
+        runTestChecksum('tests-checksum.digest', ['tests-checksum-good', 'tests-checksum-bad']),
+        {
+            instanceOf: CSErrorMismatch
+        }
+    )) as CSErrorMismatch;
+
+    t.is(error.file, 'tests-checksum-bad');
+});
+
+/*
+    No encoding specified; default to utf8
+*/
+
+test('Encoding: Default utf8', (t) => {
+    const validator = new CSVerify('sha256', 'fake.sha256sum');
+    t.is(validator.encode(false), 'utf8');
+});
+
+/*
+    encoding specified; override utf8
+*/
+
+test('Encoding: Specify override', (t) => {
+    const validator = new CSVerify('sha256', 'fake.sha256sum', {
+        encoding: 'hex'
+    });
+
+    t.is(validator.encode(false), 'hex');
+});
